7444d08a6c4ad99b082bebdc9dcf6359acc22668
juanda <juandalibaba@gmail.com>
2013-09-24 17:46:43 +0200

    seguimos limpiando

--- src/Jazzyweb/Bioritmos/Views/layout.php
+++ src/Jazzyweb/Bioritmos/Views/layout.php
@@ -4,15 +4,11 @@
     <meta charset="utf-8">
     <title>Bioritmos</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!-- Loading Bootstrap -->
-    <link href="<?php echo asset('bootstrap/css/bootstrap.css') ?>" rel="stylesheet">
-
+    <link href="<?php echo asset('css/bootstrap.css') ?>" rel="stylesheet">
     <!-- Loading Flat UI -->
     <link href="<?php echo asset('css/flat-ui.css') ?>" rel="stylesheet">
-
     <link rel="shortcut icon" href="<?php asset('images/favicon.ico') ?>">
-
 </head>
 <body>
 <div class="container">
@@ -20,7 +16,6 @@
 
 <?php echo $templateContent ?>
 </div> <!-- /container -->
-
 
 <!-- Load JS here for greater good =============================-->
 <script src="<?php echo asset('js/jquery-1.8.3.min.js') ?>"></script>
