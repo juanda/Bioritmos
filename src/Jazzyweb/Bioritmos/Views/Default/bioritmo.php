b0a008a467bffc68032a9da1c02887c4ffd7874c
juanda <juandalibaba@gmail.com>
2013-09-24 18:32:41 +0200

    putos colores

--- src/Jazzyweb/Bioritmos/Views/Default/bioritmo.php
+++ src/Jazzyweb/Bioritmos/Views/Default/bioritmo.php
@@ -1,6 +1,7 @@
+
 <div class="login">
         <img src="<?php echo asset($file)?>" />
-    <div class="span10">
-    <a href="<?php echo url('/') ?>" class="btn btn-danger">Otro Bioritmo</a>
-        </div>
+    <div class="row-fluid">
+        <a href="<?php echo url('/') ?>" class="btn btn-danger">Otro Bioritmo</a>
+    </div>
 </div>
